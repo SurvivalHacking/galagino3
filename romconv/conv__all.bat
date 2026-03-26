e4b2af8b836f6f0be000899e0b10d3258a8e1655
Davide Gatti <76624331+SurvivalHacking@users.noreply.github.com>
2026-03-26 20:59:43 +0100

    Rom Converter V3.2

---
+++ romconv/conv__all.bat
@@ -0,0 +1,32 @@
+@echo off
+echo --------- Convert all ---------
+
+call conv_z80.bat
+call conv_pacman.bat
+call conv_galaga.bat
+call conv_digdug.bat
+call conv_frogger.bat
+call conv_dkong.bat
+call conv_1942.bat
+call conv_lizwiz.bat
+call conv_eyes.bat
+call conv_mrtnt.bat
+call conv_theglob.bat
+call conv_crush.bat
+call conv_anteater.bat
+call conv_bombjack.bat
+call conv_mrdo.bat
+call conv_bagman.bat
+call conv_pengo.bat
+call conv_mspacman.bat
+call conv_ladybug.bat
+call conv_tutankhm.bat
+call conv_gyruss.bat
+call conv_galaxian.bat
+call conv_invaders.bat
+call conv_timeplt.bat
+
+echo ---- Important
+echo ---- Please check if any errors occured!
+echo ---- Important
+Pause
